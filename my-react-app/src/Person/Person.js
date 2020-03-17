135f2777afbb3cf2c0043c5ed7961f05d8e2775b
GustavoBarcaro <gubarcaro@gmail.com>
2020-03-17 15:55:02 -0300

    testes

---
+++ my-react-app/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const person = () => {
+    return (
+        <div className="App">
+            <form>
+                <div><input type="text"></input></div>
+                <div><textarea></textarea></div>
+                
+                <button>Enviar</button>
+            </form>
+        </div>
+      );
+}
+
+export default person;
