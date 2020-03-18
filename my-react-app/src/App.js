b77001c8ac5ac143a80f92635e98bed14a6cbdee
Gyubido <52919597+GustavoBarcaro@users.noreply.github.com>
2020-03-17 23:55:34 -0300

    mais testes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,18 +2,75 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person  from './Person/Person';
+import Button from '@material-ui/core/Button';
+import FindReplaceIcon from '@material-ui/icons/FindReplace';
+
+
 
 class App extends Component{
+	state = {
+		persons: [
+			{
+				name: "Gustavo",
+				age: 18
+			},
+			{
+				name: "Guilherme",
+				age: 18
+			},
+			{
+				name: "Matheus",
+				age: 28
+			},
+			{
+				name: "Jean",
+				age: 30
+      }
+		]
+  }
+  
+  switchNameHandler = () =>{
+    // console.log("was clicked");
+    // Não usar isso: this.state.persons[0].name = "Gustavo Barcaro"
+    this.setState({
+      persons:[
+        {
+          name: "Gustavo Barcaro",
+          age: 18
+        },
+        {
+          name: "Guilherme Ribeiro",
+          age: 18
+        },
+        {
+          name: "Isadora Bedanni",
+          age: 28
+        }
+      ]
+    });
+  }
 	render () {
 		return (
 		  <div className="App">
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
 			  <h1>Hi, I'm a React App</h1>
 			  <p>This is really working!!</p>
-			  <Person></Person>
+			  <Button 
+         variant="contained"
+         color="primary" 
+         onClick={this.switchNameHandler}
+         endIcon = {<FindReplaceIcon/>}>
+          Switch name
+        </Button>
+			  <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Programing</Person>
+			  <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Playing</Person>
+			  <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
 		  </div>
 		);
 		//return React.createElement('div', {className: 'App'},React.createElement('h1', null, 'Do it work??'))
 	  }
 }
-
 export default App;
--- my-react-app/src/Person/Person.js
+++ my-react-app/src/Person/Person.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
-const person = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'block',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: '10px auto',
+      padding: "10px",
+      width: theme.spacing(50),
+      height: theme.spacing(16),
+      position: 'relative'
+    },
+  },
+}));
+const Person = props => {
+  const classes = useStyles();
+  const [codeState, setCodeState] = useState({
+    code: Math.floor(Math.random() * 1250000)
+  });
     return (
-        <div className="App">
-            <form>
-                <div><input type="text"></input></div>
-                <div><textarea></textarea></div>
-                
-                <button>Enviar</button>
-            </form>
+        <div className={classes.root}>
+        <Paper elevation={3} square={false}>
+            <p>I'm a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+            <p><b>My Code: </b>{codeState.code}</p>
+        </Paper>
         </div>
       );
 }
 
-export default person;
+export default Person;
