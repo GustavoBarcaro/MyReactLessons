509ac9d47527af2126460da9c7c9ebe5b0ca99f2
Gyubido <52919597+GustavoBarcaro@users.noreply.github.com>
2020-03-27 15:10:15 -0300

    cockpit

---
+++ my-react-app/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import styles from './Cockpit.module.css'
+
+const cockpit = (props) => {
+    let classes = [];
+    if(props.persons.length <= 2){
+      classes.push(styles.red);
+    }
+    if(props.persons.length <= 1) {
+      classes.push(styles.bold);
+    }
+    return ( <div className={props.show ? styles.CockpitPink : styles.Cockpit}>
+
+                <h1>Hi, I'm a React App</h1>
+                <p className={classes.join(' ')} >This is really working!!</p>
+                <Button 
+                    variant="contained"
+                    color={props.show ? "secondary" : "primary"} 
+                    onClick={props.clicked}
+                    endIcon = {props.show ? <VisibilityOffIcon/> : <VisibilityIcon/>}
+                    >
+                    { props.show ? 'Hide names' : 'Show names'}
+                </Button>
+            </div>);
+};
+
+export default cockpit;
--- my-react-app/src/containers/App.js
+++ my-react-app/src/containers/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import styles from './App.module.css';
-import Person  from '../components/Persons/Person/Person';
-import Button from '@material-ui/core/Button';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component{
 	state = {
@@ -45,51 +43,22 @@ togglePersonsHandler = () =>{
 }
  
 	render () {
-  
     let persons = null;
     if(this.state.show){
-      persons = (
-        <div>
-        {
-          this.state.persons.map((person, index) =>{
-            return <Person 
-            name={person.name} 
-            age={person.age}
-            click={() => this.delelePersonHandler(index)}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}
-            />
-          })
-        }
-        </div> 
-      );
-    }
-    let classes = [];
-    if(this.state.persons.length <= 2){
-      classes.push(styles.red);
-    }
-    if(this.state.persons.length <= 1) {
-      classes.push(styles.bold);
-    }
+      persons = <Persons 
+                  persons={this.state.persons}
+                  clicked={this.delelePersonHandler}
+                  changed={this.nameChangedHandler}  
+                />
+          }
 		return (
 		  <div className={styles.App}>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+        <Cockpit 
+          persons={this.state.persons}
+          show={this.state.show}
+          clicked={this.togglePersonsHandler}
         />
-			  <h1 className={this.state.show ? styles.h1Pink : styles.h1}>Hi, I'm a React App</h1>
-			  <p className={classes.join(' ')} >This is really working!!</p>
-			  <Button 
-         variant="contained"
-         color={this.state.show ? "secondary" : "primary"} 
-         onClick={this.togglePersonsHandler}
-         endIcon = {this.state.show ? <VisibilityOffIcon/>: <VisibilityIcon/>}
-         >
-          { this.state.show ? 'Hide names' : 'Show names'}
-        </Button>
-        {
-          persons
-        }
+        { persons }
 		  </div>
 		);
 	}
