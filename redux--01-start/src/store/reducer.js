c9d504a2518dc38b2dd78424d60a86d2deff5472
Gyubido <gubarcaro@gmail.com>
2020-04-28 23:39:28 -0300

    actions

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,30 +1,54 @@
+import * as actionTypes from './actions'
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
 
-    if(action.type === 'INCREMENT'){
+    if(action.type === actionTypes.INCREMENT){
         return {
+            ...state,
             counter: state.counter + 1
         }
     }
 
-    if(action.type === 'DECREMENT'){
+    if(action.type === actionTypes.DECREMENT){
         return {
+            ...state,
             counter: state.counter - 1
         }
     }
 
-    if(action.type === 'ADD'){
+    if(action.type === actionTypes.ADD){
         return {
+            ...state,
             counter: state.counter + action.value
         }
     }
 
-    if(action.type === 'SUBTRACT'){
+    if(action.type === actionTypes.SUBTRACT){
         return {
-            counter: state.counter -action.value
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
+
+    if(action.type === actionTypes.STORE_RESULT){
+        return {
+            ...state,
+            results: state.results.concat({id: new Date() ,value: state.counter})
+        }
+    }
+
+    if(action.type === actionTypes.DELETE_RESULT){
+        // const newArray = [...state.results];
+        // newArray.splice(action.id, 1);
+        const updatedArray = state.results.filter(el => el.id !== action.id )
+        return {
+            ...state,
+            results: updatedArray
         }
     }
 
